a4a9526b4c2b22881b7b6bc9716a35544adf67e1
Woohyun Shin <woo_hyun_1@naver.com>
2022-08-30 07:50:41 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ export default function App() {
   const [item, setItem] = useState(1);
   const increment = () => setItem(item + 1);
   const decrement = () => setItem(item - 1);
+  console.log("Hi")
   return (
     <div className="App">
       <h1>Hello {item}</h1>
