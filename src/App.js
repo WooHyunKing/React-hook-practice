78a9e418e99777b62e5c157ca62422814bb58134
Woohyun Shin <87849920+WooHyunKing@users.noreply.github.com>
2022-08-30 23:39:03 +0900

    Learn about useTitle

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 //useInput을 사용하면 다른 함수에서 이벤트를 처리할 수 있음
-// const useInput = (initialValue, validator) => {
-//   const [value, setValue] = useState(initialValue);
-//   const onChange = (event) => {
-//     let willUpdate = true;
-//     if (typeof validator === "function") {
-//       willUpdate = validator(value);
-//     }
-//     if (willUpdate) {
-//       setValue(event.target.value);
-//     } else {
-//       setValue(event.target.value.slice(0, -2));
-//       alert("재입력하세요");
-//     }
-//   };
-//   return { value, onChange };
-// };
-
-const content=[
-    {
-        tab:"Section 1",
-        content:"I'm the content of the Section 1"
-    },
-    {
-        tab:"Section 2",
-        content:"I'm the content of the Section 2"
-    },
-]
-
-const useTabs=(initialTab,allTabs)=>{
-    if(!allTabs || !Array.isArray(allTabs)){
-        return;
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
     }
-    const [currentIndex,setCurrentIndex]=useState(initialTab);
-    return{
-        currentItem:allTabs[currentIndex],
-        changeItem:setCurrentIndex
+    if (willUpdate) {
+      setValue(event.target.value);
+    } else {
+      setValue(event.target.value.slice(0, -2));
+      alert("재입력하세요");
     }
-}
+  };
+  return { value, onChange };
+};
+
+// const content=[
+//     {
+//         tab:"Section 1",
+//         content:"I'm the content of the Section 1"
+//     },
+//     {
+//         tab:"Section 2",
+//         content:"I'm the content of the Section 2"
+//     },
+// ]
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
 
 export default function App() {
-    const {currentItem,changeItem}=useTabs(0,content);
   //Hooks가 생기기전까지는 state를 함수형 컴포넌트에서 사용할 수 없었음(클래스형 컴포넌트에서만 가능)
-  const sayHello = () => console.log("hello");
-  const [number,setNumber]=useState(0);
-  const [aNumber,setAnumber]=useState(0);
-  
-  useEffect(sayHello,[number]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={()=>setNumber(number+1)}>{number}</button>
-      <button onClick={()=>setAnumber(aNumber+1)}>{aNumber}</button>
-
     </div>
   );
 }
