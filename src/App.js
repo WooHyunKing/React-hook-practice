b71aae22017c468146bda43249350d79f8ba1fc5
Woohyun Shin <87849920+WooHyunKing@users.noreply.github.com>
2022-08-30 16:47:11 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+//useInput을 사용하면 다른 함수에서 이벤트를 처리할 수 있음
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(event.target.value);
+    } else {
+      setValue(event.target.value.slice(0, -2));
+      alert("재입력하세요");
+    }
+  };
+  return { value, onChange };
+};
+
+export default function App() {
+  //Hooks가 생기기전까지는 state를 함수형 컴포넌트에서 사용할 수 없었음(클래스형 컴포넌트에서만 가능)
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  const [item, setItem] = useState(1);
+  const increment = () => setItem(item + 1);
+  const decrement = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      <input placeholder="Name" {...name} /> {/*  위 코드와 동일 */}
+    </div>
+  );
+}
+
+// class AppUgly extends React.Component{ //Hooks를 사용하지 않은 클래스형 컴포넌트
+//   state={
+//     item:1
+//   };
+
+//   incrementItem=()=>{
+//     this.setState(state=>{
+//       return{
+//         item:state.item+1
+//       };
+//     });
+//   };
+
+//   decrementItem=()=>{
+//     this.setState(state=>{
+//       return{
+//         item:state.item-1
+//       };
+//     });
+//   };
+
+//   render(){
+//     const {item}=this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello {item}</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={this.incrementItem}>Increment</button>
+//         <button onClick={this.AppdecrementItem}>Decrement</button>
+//       </div>
+//     );
+//   }
+// }
