9969456fd3051d433991a1805a1a4910a7c7416e
Woohyun Shin <woo_hyun_1@naver.com>
2022-08-30 08:24:55 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,39 +2,66 @@ import React, { useState } from "react";
 import "./styles.css";
 
 //useInput을 사용하면 다른 함수에서 이벤트를 처리할 수 있음
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
+// const useInput = (initialValue, validator) => {
+//   const [value, setValue] = useState(initialValue);
+//   const onChange = (event) => {
+//     let willUpdate = true;
+//     if (typeof validator === "function") {
+//       willUpdate = validator(value);
+//     }
+//     if (willUpdate) {
+//       setValue(event.target.value);
+//     } else {
+//       setValue(event.target.value.slice(0, -2));
+//       alert("재입력하세요");
+//     }
+//   };
+//   return { value, onChange };
+// };
+
+const content=[
+    {
+        tab:"Section 1",
+        content:"I'm the content of the Section 1"
+    },
+    {
+        tab:"Section 2",
+        content:"I'm the content of the Section 2"
+    },
+]
+
+const useTabs=(initialTab,allTabs)=>{
+    if(!allTabs || !Array.isArray(allTabs)){
+        return;
     }
-    if (willUpdate) {
-      setValue(event.target.value);
-    } else {
-      setValue(event.target.value.slice(0, -2));
-      alert("재입력하세요");
+    const [currentIndex,setCurrentIndex]=useState(initialTab);
+    return{
+        currentItem:allTabs[currentIndex],
+        changeItem:setCurrentIndex
     }
-  };
-  return { value, onChange };
-};
+}
 
 export default function App() {
+    const {currentItem,changeItem}=useTabs(0,content);
   //Hooks가 생기기전까지는 state를 함수형 컴포넌트에서 사용할 수 없었음(클래스형 컴포넌트에서만 가능)
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
-  const [item, setItem] = useState(1);
-  const increment = () => setItem(item + 1);
-  const decrement = () => setItem(item - 1);
-  console.log("Hi")
+//   const maxLen = (value) => value.length <= 10;
+//   const name = useInput("Mr.", maxLen);
+//   const [item, setItem] = useState(1);
+//   const increment = () => setItem(item + 1);
+//   const decrement = () => setItem(item - 1);
+//   console.log("Hi")
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <h2>Start editing to see some magic happen!</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <input placeholder="Name" value={name.value} onChange={name.onChange} />
-      <input placeholder="Name" {...name} /> {/*  위 코드와 동일 */}
+      <input placeholder="Name" {...name} />  위 코드와 동일 */}
+      
+      {content.map((section,index)=><button onClick={()=>changeItem(index)}>{section.tab}</button>)}
+      <div>{currentItem.content}</div>
+      
+      
     </div>
   );
 }
